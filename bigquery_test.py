35a0e735d1cbaff1689d2fa1b7e9cb235f6adb5f
nhathiep <truong.nhathiep@gmail.com>
2022-01-08 02:13:58 +1100

    bigquery files chnages - added 'export credentials' comments and cleaned up unecessary snippets

--- bigquery_test.py
+++ bigquery_test.py
@@ -1,3 +1,7 @@
+# Authentification is required by exporting the credentials contained in the json file
+# The line below needs to be executed in the Terminal before runing the python file: 
+# export GOOGLE_APPLICATION_CREDENTIALS= "/Users/nhathiep/Python_Projects/SQL connectors/proud-outrider-337507-319bec3fd79c.json"
+
 from google.cloud import bigquery
 client = bigquery.Client()
 
@@ -7,7 +11,19 @@ QUERY = (
     'WHERE state = "TX" '
     'LIMIT 10')
 query_job = client.query(QUERY)  # API request
-rows = query_job.result()  # Waits for query to finish
+#rows = query_job.result()  # Waits for query to finish
 
-for row in rows:
-    print(row.name)
+#for row in rows:
+#    print(row.name)
+
+dataframe = (
+    query_job
+    .result()
+    .to_dataframe(
+        # Optionally, explicitly request to use the BigQuery Storage API. As of
+        # google-cloud-bigquery version 1.26.0 and above, the BigQuery Storage
+        # API is used by default.
+        create_bqstorage_client=True,
+    )
+)
+print(dataframe.head())
